2bfd3e0aba0111d6993fc642f4569db3425c768d
panni <panni@fragstore.net>
2018-04-20 18:40:17 +0200

    try using local 24h/12h time format depending on Kodi settings; partially fixes #90

--- lib/windows/seekdialog.py
+++ lib/windows/seekdialog.py
@@ -486,7 +486,11 @@ class SeekDialog(kodigui.BaseDialog):
         to = self.trueOffset()
         self.setProperty('time.current', util.timeDisplay(to))
         self.setProperty('time.left', util.timeDisplay(self.duration - to))
-        self.setProperty('time.end', time.strftime('%I:%M %p', time.localtime(time.time() + ((self.duration - to) / 1000))).lstrip('0'))
+
+        _fmt = '%I:%M %p'
+        if util.get24hFormat():
+            _fmt = '%H:%M'
+        self.setProperty('time.end', time.strftime(_fmt, time.localtime(time.time() + ((self.duration - to) / 1000))).lstrip('0'))
 
     def seekForward(self, offset):
         self.selectedOffset += offset
