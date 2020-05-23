a736c0098034c6772bb15241e30b4f589dd539e1
panni <panni@fragstore.net>
2020-05-23 05:18:24 +0200

    fix bad merge

--- lib/util.py
+++ lib/util.py
@@ -102,7 +102,7 @@ class AdvancedSettings(object):
         ("background_art_blur_amount", 128),
         ("background_art_opacity_amount", 60),
         ("screensaver_quiz", False),
-        ("intro_skip_early", False)
+        ("intro_skip_early", False),
         ("postplay_always", False),
         ("postplay_timeout", 16),
     )
