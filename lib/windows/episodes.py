ca69d7826c12169a9a4399db4a4c7522a6996849
pannal <panni@fragstore.net>
2023-12-15 19:34:42 +0100

    Episodes: episodesPaginator can be None onReinit

--- lib/windows/episodes.py
+++ lib/windows/episodes.py
@@ -286,7 +286,7 @@ class EpisodesWindow(kodigui.ControlledWindow, windowutils.UtilMixin, SeasonsMix
         self.selectEpisode()
 
         mli = self.episodeListControl.getSelectedItem()
-        if not mli:
+        if not mli or not self.episodesPaginator:
             return
 
         reloadItems = [mli]
