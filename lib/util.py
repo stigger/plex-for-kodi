8618389ee91e32551c40971dd9836c6f44412453
pannal <panni@fragstore.net>
2023-12-22 19:31:32 +0100

    Core: Fix addon crash on OSMC due to System.BuildVersion not matching Kodi's default Core: Log Kodi version and build number

--- lib/util.py
+++ lib/util.py
@@ -30,14 +30,27 @@ ADDON = xbmcaddon.Addon()
 
 SETTINGS_LOCK = threading.Lock()
 
-# buildversion looks like: XX.X[-TAG] (a+.b+.c+) (.+)
-_ver, _build = xbmc.getInfoLabel('System.BuildVersion').split()[:2]
+_build = None
+# buildversion looks like: XX.X[-TAG] (a+.b+.c+) (.+); there are kodi builds that don't set the build version
+sys_ver = xbmc.getInfoLabel('System.BuildVersion')
+_ver = sys_ver
+if ' ' in sys_ver and '(' in sys_ver:
+    _ver, _build = sys_ver.split()[:2]
+
 _splitver = _ver.split(".")
-KODI_VERSION_MAJOR, KODI_VERSION_MINOR = int(_splitver[0].split("-")[0]), int(_splitver[1].split("-")[0])
+KODI_VERSION_MAJOR, KODI_VERSION_MINOR = int(_splitver[0].split("-")[0].strip()), \
+                                         int(_splitver[1].split("-")[0].strip())
+
+_bmajor, _bminor, _bpatch = (KODI_VERSION_MAJOR, KODI_VERSION_MINOR, 0)
+if _build:
+    _bmajor, _bminor, _bpatch = _build[1:-1].split(".")
+else:
+    xbmc.log('script.plex: Couldn\'t determine build version, falling back to Kodi version', xbmc.LOGINFO)
 
 # calculate a comparable build number
-_bmajor, _bminor, _bpatch = _build[1:-1].split(".")
 KODI_BUILD_NUMBER = int("{0}{1:02d}{2:03d}".format(_bmajor, int(_bminor), int(_bpatch)))
+xbmc.log('script.plex: Kodi {0}.{1} (build {2})'.format(KODI_VERSION_MAJOR, KODI_VERSION_MINOR, KODI_BUILD_NUMBER),
+         xbmc.LOGINFO)
 
 
 if KODI_VERSION_MAJOR > 18:
