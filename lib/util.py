30c48bdbeada6a9d0f60b646cdca280be0ee6e12
pannal <panni@fragstore.net>
2023-12-23 03:48:40 +0100

    Core: Advanced Settings: set default resolution scale to 200% (300% recommended)

--- lib/util.py
+++ lib/util.py
@@ -170,7 +170,7 @@ class AdvancedSettings(object):
         ("dbg_crossfade", True),
         ("subtitle_use_extended_title", True),
         ("dialog_flicker_fix", True),
-        ("poster_resolution_scale_perc", 300),
+        ("poster_resolution_scale_perc", 200),
     )
 
     def __init__(self):
