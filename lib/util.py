eddaa5c172bf1a9294110981a32a4b15d11d4903
pannal <panni@fragstore.net>
2024-01-17 18:02:05 +0100

    Core: Fall back properly when Kodi version string couldn't be parsed

--- lib/util.py
+++ lib/util.py
@@ -23,6 +23,7 @@ from kodi_six import xbmcaddon
 from kodi_six import xbmcvfs
 
 from . import colors
+# noinspection PyUnresolvedReferences
 from .exceptions import NoDataException
 from plexnet import signalsmixin, plexapp
 
@@ -37,17 +38,29 @@ _build = None
 # buildversion looks like: XX.X[-TAG] (a+.b+.c+) (.+); there are kodi builds that don't set the build version
 sys_ver = xbmc.getInfoLabel('System.BuildVersion')
 _ver = sys_ver
-if ' ' in sys_ver and '(' in sys_ver:
-    _ver, _build = sys_ver.split()[:2]
 
-_splitver = _ver.split(".")
-KODI_VERSION_MAJOR, KODI_VERSION_MINOR = int(_splitver[0].split("-")[0].strip()), \
-                                         int(_splitver[1].split("-")[0].strip())
+try:
+    if ' ' in sys_ver and '(' in sys_ver:
+        _ver, _build = sys_ver.split()[:2]
+
+    _splitver = _ver.split(".")
+    KODI_VERSION_MAJOR, KODI_VERSION_MINOR = int(_splitver[0].split("-")[0].strip()), \
+                                             int(_splitver[1].split(" ")[0].split("-")[0].strip())
+except:
+    xbmc.log('script.plex: Couldn\'t determine Kodi version, assuming 19.4. Got: {}'.format(sys_ver))
+    # assume something "old"
+    KODI_VERSION_MAJOR = 19
+    KODI_VERSION_MINOR = 4
 
 _bmajor, _bminor, _bpatch = (KODI_VERSION_MAJOR, KODI_VERSION_MINOR, 0)
+parsedBuild = False
 if _build:
-    _bmajor, _bminor, _bpatch = _build[1:-1].split(".")
-else:
+    try:
+        _bmajor, _bminor, _bpatch = _build[1:-1].split(".")
+        parsedBuild = True
+    except:
+        pass
+if not parsedBuild:
     xbmc.log('script.plex: Couldn\'t determine build version, falling back to Kodi version', xbmc.LOGINFO)
 
 # calculate a comparable build number
